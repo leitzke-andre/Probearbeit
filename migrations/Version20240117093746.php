00e6467401133305eb4d520b9d40b9266bd85d55
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2024-01-17 23:11:41 +0100

    Removed unnecessary table from migration. It was only done as a change to an existing migration, instead of a new migration, due to the nature of this code challenge. In a production environment a new migration would be created to drop the table instead.

--- migrations/Version20240117093746.php
+++ migrations/Version20240117093746.php
@@ -22,7 +22,6 @@ final class Version20240117093746 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE work_unit (id INT AUTO_INCREMENT NOT NULL, project_id INT NOT NULL, start DATETIME NOT NULL, end DATETIME NOT NULL, UNIQUE INDEX UNIQ_2578BD6B166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE work_unit ADD CONSTRAINT FK_2578BD6B166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
     }
 
@@ -32,6 +31,5 @@ final class Version20240117093746 extends AbstractMigration
         $this->addSql('ALTER TABLE work_unit DROP FOREIGN KEY FK_2578BD6B166D1F9C');
         $this->addSql('DROP TABLE project');
         $this->addSql('DROP TABLE work_unit');
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
